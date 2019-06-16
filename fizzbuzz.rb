d4cba8d0f4dcf920d250721bb1c6c2a6d60be6d9
dynamic-router-3131 <jtboss47@gmail.com>
2019-06-16 15:22:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,26 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  
+  isThree = false
+  isFive = false
+  
+  if (num % 3 == 0) 
+    isThree = true
+    print "Fizz"
+  end 
+  
+  if(num % 5 == 0)
+    isFive = true
+    print "Buzz"
+  end
+  
+  if(isFive == true && isThree == true)
+    print "Fizzbuzz"
+  end
+  
+end
+
+fizzbuzz(5)
+
